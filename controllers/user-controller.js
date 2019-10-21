30754311f676595697dd3e0f445ee509651b509d
lordviet <viet1231@abv.bg>
2019-10-21 20:34:21 +0300

    created register functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,4 @@
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
 
 module.exports = {
     getLogin: function (req, res) {
@@ -11,37 +10,26 @@ module.exports = {
     postRegister: function (req, res, next) {
         const { username, password, repeatPassword } = req.body;
         if (password !== repeatPassword) {
-            return res.status(400).send({
-                message: "Password must match repeat password"
-            });
+            return res.render("register", {
+                errors: {
+                    repeatPassword: "Password and repeat password don't match"
+                }
+            })
         }
 
-        bcrypt.hash(password, 10, (err, hash) => {
-            if (err) { next(err); return; }
-            // adding users to db
-            // TODO handle duplicates
-            User.create({ username, password: hash })
-                .then(() => {
-                    return res.redirect("/login");
+        User.create({ username, password }).then(() => {
+            return res.redirect("/login");
+        }).catch(err => {
+            if (err.name === "MongoError" && err.code === 11000) {
+                return res.render("register", {
+                    errors: {
+                        username: "Username already exists!"
+                    }
                 });
-
-            // catch unique
+                // return;
+            }
+            next(err);
         });
 
-    }
-}
-
-function auth(authLevel) {
-    return (req, res, next) => {
-        const token = req.cookies['auth_cookie'];
-        // const authUser = users.find(user => user.id === req.session.userId);
-        const data = jwt.verify(token, secret);
-        const authUser = users.find(user => user.id === data.userId);
-        if (!authUser) {
-            res.status(401).send('401');
-            return;
-        }
-        req.user = authUser;
-        next();
     }
 }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -11,6 +13,27 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.methods = {
+    matchPassword: function (password) {
+        return bcrypt.compare(password, this.password);
+    }
+}
+
+userSchema.pre("save", function (next) {
+    if (this.isModified("password")) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) { next(err); return; }
+            bcrypt.hash(this.password, salt, (err, hash) => {
+                if (err) { next(err); return; }
+                this.password = hash;
+                next();
+            });
+        })
+        return;
+    }
+    next();
 });
 
 
